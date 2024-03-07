5d5447c5eb91cdf305b0ed56bc0b012e40c4fc9d
vithorkr <162274489+vithorkr@users.noreply.github.com>
2024-03-07 00:45:00 -0300

    Add files via upload

---
+++ test/runtests.jl
@@ -0,0 +1,13 @@
+using Test
+using MiscellaneaFunctions
+
+println("5! = $(nfact(5,1))")
+println("6! = $(nfact(6,1))")
+println("7! = $(nfact(7,1))")
+println("0! = $(nfact(0,1))")
+println("2!! = $(nfact(2,2))")
+println("3!!! = $(nfact(3,3))")
+println("4!!!! = $(nfact(4,4))")
+println("80!(7) = $(nfact(80,7))")
+println("0!(10) = $(nfact(0,10))")
+println("100!(200) = $(nfact(100,200))")
